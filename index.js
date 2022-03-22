52ebb8b2c98abd30f27c31a1608e92f2a1eee47f
胡燕燕 <hyanyan@northcpt.com>
2022-03-22 21:42:17 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,113 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const fs = require('fs');
+// 解析request的body的功能(post请求)
+const bodyParser = require('koa-bodyparser');
+const { v4: uuidv4 } = require('uuid');
+
+const app = new Koa();
+const router = new Router();
+let fileName = 'database.json';
+let type = 'utf8';
+let list = JSON.parse(fs.readFileSync(fileName, type));
+
+// app.use(async (ctx) => {
+// 	ctx.body = 'hello koa2'
+
+// })
+// let list = [
+// 	{
+// 		name: 'zhangsan',
+// 		id: '1',
+// 		age: 12,
+// 	},
+// 	{
+// 		name: 'lisi',
+// 		id: '2',
+// 		age: 14,
+// 	},
+// 	{
+// 		name: 'wangwu',
+// 		id: '3',
+// 		age: 17,
+// 	},
+// ];
+// add url-route:
+router.get('/getName', async (ctx, next) => {
+	const { name, page, size } = ctx.query;
+	let message = 'no name';
+	// 有参数则查询，没有则查全部
+	if (name) {
+		let item = list.find((it) => {
+			return it.name === name;
+		});
+		// 区分是否有当前的名称数据，有就返回，没有则返回 no found
+		if (item) {
+			ctx.body = item;
+			return;
+		} else {
+			message = 'no found';
+			ctx.body = { errorMessage: message };
+			return;
+		}
+	} else {
+		// ctx.body = list;
+		// return;
+	}
+
+	// 分页处理 page、size有效的情况下查询
+	// splice()处理
+	if (page > 0 && size > 0) {
+		let newList = [...list];
+		const allSize = Number(size);
+		const allPage = Number(page);
+		const index = (allPage - 1) * allSize;
+		let lenth = allPage * allSize < list.length ? allSize : list.length;
+		let result = newList.splice(index, lenth);
+		ctx.body = result;
+		return;
+	}
+
+	ctx.body = list;
+});
+
+router.post('/addName', async (ctx, next) => {
+	let items = ctx.request.body;
+	items.id = uuidv4();
+	list = [...list, items];
+	fs.writeFileSync(fileName, JSON.stringify(list, null, 2));
+	ctx.body = list;
+});
+
+router.delete('/delName/:id', async (ctx, next) => {
+	const id = ctx.params.id;
+	list = list.filter((item) => {
+		return item.id !== id;
+	});
+	fs.writeFileSync(fileName, JSON.stringify(list, null, 2));
+	ctx.body = { message: 'success', list };
+});
+
+router.put('/name/:id', async (ctx, next) => {
+	const id = ctx.params.id;
+	let items = ctx.request.body;
+	let index = list.findIndex((item) => item.id === id);
+
+	if (index >= 0) {
+		items.age = Number(items.age);
+		list[index] = { ...list[index], ...items };
+		fs.writeFileSync(fileName, JSON.stringify(list, null, 2));
+		ctx.body = { message: 'success', list };
+	} else {
+		ctx.body = { message: 'error' };
+	}
+});
+
+//由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象上
+app.use(bodyParser());
+// add router middleware:
+app.use(router.routes());
+
+app.listen(3000);
+
+console.log(23456);
---
+++ read.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+// 同步写入方法 writeFileSync
+// fs.writeFileSync('2', 'hellos world');
+// let data = fs.readFileSync('2', 'utf8');
+
+// console.log(data);
+
+// 异步写入方法writeFile
+const fileName = 'datbase.json';
+let list = [
+	{
+		name: 'zhangsan',
+		id: '1',
+		age: 12,
+	},
+	{
+		name: 'lisi',
+		id: '2',
+		age: 14,
+	},
+	{
+		name: 'wangwu',
+		id: '3',
+		age: 17,
+	},
+];
+// const replacer = (key, value) => {
+// 	console.log(key);
+// 	return typeof value === 'string' ? value : undefined;
+// };
+// let arr = JSON.stringify(list, replacer, 2);
+// console.log(arr);
+const type = 'utf8';
+// JSON.stringify(value[, replacer [, space]])
+// fs.writeFile(fileName, JSON.stringify({ list }, null, 2), (err) => {
+// 	if (!err) {
+// 		fs.readFile(fileName, type, (err, data) => {
+// 			console.log(data);
+// 		});
+// 	}
+// });
+// fs.unlink(fileName, (err) => {
+// 	console.log(err, 1222);
+// });
+
+// 增删改查 查：readFileSync、readFile    增：writeFileSync、writeFile 、追加写入appendFile、appendFileSync 文件拷贝copyFileSync、copyFile
+// 删：unlink rmSync rm
